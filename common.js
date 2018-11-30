51e37acd78770487714542f1879fb449c70712c5
ivanovdevelop <45172327+ivanovdevelop@users.noreply.github.com>
2018-11-30 22:57:48 +0300

    Update common.js

--- common.js
+++ common.js
@@ -1,1 +1,51 @@
 
+$(function() {
+
+	$('#my-menu').mmenu({
+		 
+        
+      extensions   : [  'theme-black', 'effect-menu-slide','pagedim-black' ],
+      navbar: {
+      title: '<img src="img/logo.svg" alt="Салон красоты Смитлер">'},
+      offCanvas: { position: 'right' }
+     
+   
+	});
+/*кнопка меню*/
+
+var api = $('#my-menu').data('mmenu');
+api.bind("open:finish", function() {
+	$('.hamburger ').addClass(' is-active');
+},100);
+api.bind("close:finish", function() {
+	$('.hamburger ').removeClass(' is-active');
+},100);
+
+	$('.carousel-services').owlCarousel({
+		loop: true,
+		nav: true,
+		smartSpeed: 700,
+		navText: ['<i class="fa fa-angle-double-left"></i>','<i class="fa fa-angle-double-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			800: {
+				items: 2
+			},
+			1100: {
+				items: 3
+			}
+		}
+	});
+
+	function carouselService(){
+		$('.carousel-services-item').each(function(){
+			var ths = $(this),
+				thch = ths.find('.carousel-services-content').outerHeight();
+				ths.find('.carousel-services-image').css('min-height',thch);
+		});
+
+	}carouselService();
+});
