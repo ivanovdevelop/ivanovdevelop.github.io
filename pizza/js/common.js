f8cde4d2e8bcedf83b90a4f92a98965d58cb47da
ivanovdevelop <45172327+ivanovdevelop@users.noreply.github.com>
2019-09-17 22:49:34 +0300

    Update common.js

--- pizza/js/common.js
+++ pizza/js/common.js
@@ -7,13 +7,13 @@
  // });
 	$('.hamburger').click(function(){
 //$('.my-menu-popup').toggleClass('vis');
-		$('.my-menu, .my-menu-popup').slideToggle('100');
+		$('.my-menu, .my-menu-popup').fadeToggle('100');
 		//$('.my-menu-popup').toggleClass('vis');
 	//alert('sxcscsc');
 	});
    $
    ('.my-menu-popup').click(function() {
-        $('.my-menu, .my-menu-popup').slideToggle('100');
+        $('.my-menu, .my-menu-popup').fadeToggle('100');
     });
    
 
